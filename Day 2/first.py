619299e11670079fa5a2d0095ce39f3e752c8930
sanjidahw <sanjidahw@gmail.com>
2020-06-07 15:50:56 -0400

    First Commit

---
+++ Day 2/first.py
@@ -0,0 +1,8 @@
+#the modulo operator returns the remainder after division --> %
+
+user_input = int(input("Enter a number"))
+
+if user_input % 2 == 0:
+	print("The number is even")
+else:
+	print("The number is odd")
